9a43755a60073a836af3329e78118d725589d40a
LarsGJobloop <lars.gunnar@jobloop.no>
2024-02-27 10:48:10 +0100

    finsihed some tests for button

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -1,19 +1,30 @@
-import { render, screen, fireEvent } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
 import { Button } from "./Button";
 import style from "./Button.module.css";
 
 describe("Button", () => {
   it("should render any children passed in correctly", () => {
     // Arrange
+    render(<Button>Click Me</Button>);
+
     // Act
+
     // Assert
+    const result = screen.getByText("Click Me");
+    expect(result).not.toBeUndefined();
   });
 
   it("should apply correct styling for the info variant", () => {
     // Arrange
+    const { container } = render(<Button variant="info"></Button>);
+    const className = style.info;
+
     // Act
+
     // Assert
+    const result = container.firstChild.classList.contains(className);
+    expect(result).toBe(true);
   });
 
   it("should apply correct styling for the action variant", () => {
