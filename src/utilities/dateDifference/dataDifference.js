096c3256a75c5c366a567d8691647f769e8a5066
LarsGJobloop <lars.gunnar@jobloop.no>
2024-02-27 11:32:29 +0100

    add dateDifference utility example

---
+++ src/utilities/dateDifference/dataDifference.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @param {Date} startDate 
+ * @param {Date} endDate 
+ * @returns {[number, number, number, number]} - [dager, timer, minutter, sekunder]
+ */
+export function dateDifference(startDate, endDate) {
+  const days = ???
+  const hours = ???
+  const minutes = ???
+  const second = ???
+
+  return [
+    days,
+    hours,
+    minutes,
+    second,
+  ]
+}
---
+++ src/utilities/dateDifference/dataDifference.spec.js
@@ -0,0 +1,15 @@
+import { describe, it, expect } from "vitest";
+import { dateDifference } from "./dataDifference";
+
+describe("dataDifference", () => {
+  it("to ??? and ??? to return ???", () => {
+    const startData = new Date("2024-02-27T09:17:30.367Z");
+    const endDate = new Date("2024-02-27T09:17:31.367Z");
+
+    const expectedDifference = [0, 0, 0, 1];
+
+    const result = dateDifference(startData, endDate);
+
+    expect(result).equal(expectedDifference);
+  });
+});
