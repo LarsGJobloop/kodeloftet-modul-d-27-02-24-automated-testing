b7b2844d0e3f4a895909c70dad93489d65d3f41d
LarsGJobloop <lars.gunnar@jobloop.no>
2024-03-02 19:05:44 +0100

    update css strategy

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css";
+/**
+ * CSS Stylsheets are sensitive
+ * to the seequence they are applied in.
+ * Here we are importing globaly applicable
+ * stylesheets.
+ * Be wary of using classes from these in components
+ * as then you would have to include these files if they
+ * ever are copied over to other projects.
+ */
+import "./styles/overrides.css";
+import "./styles/palettes.css";
+import "./styles/utilities.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
